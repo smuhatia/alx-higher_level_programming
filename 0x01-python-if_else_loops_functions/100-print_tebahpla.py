7b1871dd3bbfdb3b2aeaa564d2f94c6754705a3e
smuhatia <shawnmuhatia62@gmail.com>
2023-06-14 08:52:55 +0000

    indention fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(26):
     print("{:s}".format(chr(122 - i))
-    if i % 2 == 0
-    else
-    "{:s}".format(chr(90 - i)), end="")
+        if i % 2 == 0
+            else
+                "{:s}".format(chr(90 - i)), end="")
