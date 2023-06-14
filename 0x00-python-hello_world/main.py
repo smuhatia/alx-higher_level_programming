3567dd01b289a3e2a5429153ab062fc0d2b9e485
smuhatia <shawnmuhatia62@gmail.com>
2023-06-14 05:38:16 +0000

    script that compiles a Python script file.

---
+++ 0x00-python-hello_world/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Best School")
+
+guillaume@ubuntu:~/py/0x00$ export PYFILE=main.py
+guillaume@ubuntu:~/py/0x00$ ./101-compile
+Compiling main.py ...
+guillaume@ubuntu:~/py/0x00$ ls
+101-compile  main.py  main.pyc
