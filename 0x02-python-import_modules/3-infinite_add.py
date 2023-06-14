74d489232134af4bfa95d2d1759adec985613133
smuhatia <shawnmuhatia62@gmail.com>
2023-06-14 09:17:37 +0000

    indenting fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 import sys
 
 if __name__ == "__main__":
-	arguments = sys.argv[1:]
+    arguments = sys.argv[1:]
 
-	result = sum([int(arg) for arg in arguments])
-	print("{}".format(result))
+    result = sum([int(arg) for arg in arguments])
+    print("{}".format(result))
