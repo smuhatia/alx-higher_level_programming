14d7e65a8f0e8ae8c24b19cf261d29f2562313cb
smuhatia <smuhatia>
2023-06-27 21:53:53 +0000

    class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is a module for a class square
+"""
+
+
+class Square:
+    """This is an implementation of a class square"""
+
+    def __init__(self, size):
+        """initialize a new square
+
+        Args:
+            size: The size of a new square
+        """
+        self.__size = size
