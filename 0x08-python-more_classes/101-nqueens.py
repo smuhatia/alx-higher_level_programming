a2080c26fa3443c81d1ddca2d9ef5eae74fe06eb
smuhatia <smuhatia>
2023-09-04 20:19:11 +0000

    N queens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""Solution to nqueens problem"""
+
+import sys
+
+
+def is_safe(board, row, col, n):
+    """Checks if queen is safe"""
+    # Check the same row
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_n_queens(board, col, n):
+    """Solve n queens"""
+    if col >= n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        print(solution)
+        return
+
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            solve_n_queens(board, col + 1, n)
+            board[i][col] = 0
+
+
+def main():
+    """Main function"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solve_n_queens(board, 0, n)
+
+
+if __name__ == "__main__":
+    main()
